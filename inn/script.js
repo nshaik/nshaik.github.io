a99312a880bcb6edbb402ffd9ae4e55a6a4383fd
nshaik <shaiknayeem786@gmail.com>
2014-09-30 09:09:23 +0530

    added innovation dir

---
+++ inn/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+		slideWidth: 200,
+		minSlides: 2,
+		maxSlides: 5,
+		slideMargin: 10,
+		pager: false,
+		controls: false,
+		auto:true
+	});
+});
