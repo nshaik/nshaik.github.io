0b8a8b22af1cd4d8dcf2364ee9b014f115eb0288
nshaik <shaiknayeem786@gmail.com>
2014-12-26 09:19:40 +0530

    new changes

--- hamzah/script/script.js
+++ hamzah/script/script.js
@@ -5,4 +5,9 @@ $(function(){
 		$(this).addClass('active').siblings().removeClass('active');
 		$('#MainContent_iframeWindow').attr('src',hrf);		
 	});
+	
+	$('form[name="form"]').submit(function( event ){
+		event.preventDefault();
+		location.href="http://www.hamzahacademy.com/ContactUs.aspx";
+	});
 });
